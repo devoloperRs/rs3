5aa6128e675fd1ec38ae6db412c56fe55c15a3b6
income <136902949+devoloperRs@users.noreply.github.com>
2023-10-11 23:41:56 +0300

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,18 @@
+
+
+
+
+// home style
+ScrollReveal().reveal(".t1", { reset: true, delay: 200, easing: "ease-in", interval: 300, origin: "right", opacity: 0 ,scale:1.1});
+ScrollReveal().reveal(".circle", { reset: true, delay: 200, easing: "ease-in", interval: 300, origin: "right", opacity: 0 ,scale:1.1});
+ScrollReveal().reveal(".king-img", { reset: true, delay: 200, easing: "ease-in", interval: 300, origin: "right", opacity: 0 ,scale:1.1});
+
+
+// about style
+ScrollReveal().reveal(".meten-about", { reset: true, delay: 200, easing: "ease-in", interval: 300 });
+
+ScrollReveal().reveal(".senf-chi", { reset: true, delay: 200, easing: "ease-in", interval: 300 });
+ScrollReveal().reveal(".info-senf", { reset: true, delay: 200, easing: "ease-in", interval: 300 });
+
+
+
